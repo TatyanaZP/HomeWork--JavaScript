f3acdb5ec13d07dcb4afdd5ea387617b3d4e6c26
Tetiana Cherkavska <165711384+TatyanaZP@users.noreply.github.com>
2024-09-12 21:30:36 +0200

    Add files via upload

---
+++ homework-2/1.js
@@ -0,0 +1,11 @@
+// -- 1 --
+// У вас є масив об’єктів fruts, і в кожному з них є name
+// Напишіть код, який перетворює їх в масив імен.
+//const fruts = [
+//  { id: 0, name: "Apple" },
+//  { id: 1, name: "Tomat" },
+//  { id: 2, name: "Cherry" },
+//  { id: 3, name: "Orange" },
+//];
+//console.log(fruts.filter((item) => item.name));
+//console.log(fruts.splice(name));
---
+++ homework-2/5.js
@@ -0,0 +1,11 @@
+// -- 5 --
+// Вирахуйте середній вік
+//const girls = [
+//{ age: 23, name: "Оля" },
+//{ age: 29, name: "Аня" },
+//{ age: 10, name: "Юля" },
+//{ age: 20, name: "Катя" },
+//];
+
+//let result = girls.reduce((r, i) => r + i.age, 0) / girls.length;
+//console.log(result); //20.5
