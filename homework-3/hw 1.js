e56fa392cdc1ed08d40d1141c81c89bbdf50bbaf
Tetiana Cherkavska <165711384+TatyanaZP@users.noreply.github.com>
2025-01-07 20:48:31 +0100

    Add files via upload

---
+++ homework-3/hw 1.js
@@ -0,0 +1,9 @@
+// Перепишіть функцію, використовуючи '?' або '||' function checkAge(age) { if (age > 18) { return true; } else { return confirm('Батьки дозволили?'); } }
+
+// function checkAge(age) {
+    // return age > 18 ? true : confirm('Батьки дозволили?');
+// }
+
+// function checkAge(age) {
+    // return age > 18 || confirm('Батьки дозволили?');
+// }
---
+++ homework-3/hw 2.js
@@ -0,0 +1,12 @@
+// Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b.
+// function min(a, b) {
+    // return a < b ? a : b;
+//   }
+
+//   function min(a, b) {
+    // if (a < b) {
+    //   return a;
+    // } else {
+    //   return b;
+    // }
+//   }
